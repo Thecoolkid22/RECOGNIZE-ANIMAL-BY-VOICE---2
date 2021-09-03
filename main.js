969b91b96936543a3432cf686e377b1546d74dd1
Thecoolkid22 <77620567+Thecoolkid22@users.noreply.github.com>
2021-09-03 20:37:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/y9ZkhnHpf/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+    console.log('Got result')
+}
